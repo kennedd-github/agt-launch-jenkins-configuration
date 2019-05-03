7c2a0f04577921f33a2848605d8bb9b1a12f4942
kennedd-github <david.kennedy@mail.com>
2019-05-03 16:49:19 +0000

    Jenkins Configuration Backup 20190503_164809

---
+++ Jenkins/workspace/agtl-jenkins-launch-config-groovy@tmp/durable-45c9745e/jenkins-wrap.bat
@@ -0,0 +1,3 @@
+@echo off 
+cmd /c ""C:\Program Files (x86)\Jenkins\workspace\agtl-jenkins-launch-config-groovy@tmp\durable-45c9745e\jenkins-main.bat"" > "C:\Program Files (x86)\Jenkins\workspace\agtl-jenkins-launch-config-groovy@tmp\durable-45c9745e\jenkins-log.txt" 2>&1
+echo %ERRORLEVEL% > "C:\Program Files (x86)\Jenkins\workspace\agtl-jenkins-launch-config-groovy@tmp\durable-45c9745e\jenkins-result.txt"
